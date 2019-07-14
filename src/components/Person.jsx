c582ce7777648f1f76a84182bcfe80b66a49caad
h2ash <hun22ash@gmail.com>
2019-07-14 18:21:06 +0300

    Added checking for className for born and died. Finished 3d step

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
-const Person = ({currentPerson, index}) => (
+
+
+const Person = ({ currentPerson, index }) => (
   <tr key={index}>
     <td>{index}</td>
-    <td>{currentPerson.name}</td>
-    <td className={currentPerson.sex === 'f' 
-          ? 'person--female' 
-          : 'person--male'}
+    <td
+      className={`
+        ${currentPerson.born < 1650 ? 'people__bord-before-1650' : ''}
+        ${currentPerson.died > 1800 ? 'people__died-after-1800' : ''}
+      `}
+      >
+      {currentPerson.name}
+    </td>
+    <td className={currentPerson.sex === 'f'
+      ? 'person--female'
+      : 'person--male'}
     >
       {currentPerson.sex}
     </td>
